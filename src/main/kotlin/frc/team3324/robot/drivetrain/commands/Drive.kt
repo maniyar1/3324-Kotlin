d9411ffa6a9d4eea14922b78967975f73ad12a74
Mani Kamali <kamalisarvestani.1@themetroschool.org>
2018-12-13 14:27:18 -0500

    Tested code and made minor adjustments
    Ran and tested curvature drive on normal robot. Last tested on RT DT.

--- src/main/kotlin/frc/team3324/robot/drivetrain/commands/Drive.kt
+++ src/main/kotlin/frc/team3324/robot/drivetrain/commands/Drive.kt
@@ -4,8 +4,10 @@ import edu.wpi.first.wpilibj.GenericHID
 import edu.wpi.first.wpilibj.command.Command
 import frc.team3324.robot.util.OI
 import frc.team3324.robot.drivetrain.DriveTrain
+import frc.team3324.robot.Robot
 
 object Drive : Command("Drive") {
+    var gyroCheck = true
     init {
         requires(DriveTrain)
     }
@@ -17,6 +19,14 @@ object Drive : Command("Drive") {
             DriveTrain.mDrive.curvatureDrive(leftY, rightX, true)
         } else if (leftY < 0.2) {
             DriveTrain.mDrive.curvatureDrive(leftY, rightX, true)
+        } else if (OI.PRIMARY_BUTTON_A.get()) {
+            var gyroGoal: Double = 0.0
+            if (gyroCheck == true) {
+                gyroGoal = DriveTrain.gyro.yaw
+                gyroCheck = false
+            }
+            val gyroReading = DriveTrain.gyro.yaw
+            val error = gyroGoal - gyroReading
         } else {
             DriveTrain.mDrive.curvatureDrive(leftY, rightX, false)
         }
